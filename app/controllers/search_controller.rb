d05ad076ad5c9b5dd9d8ffb487a38c2d02b5e4d2
Lane Winham <mlwinham12@gmail.com>
2016-09-20 10:16:25 -0600

    working on data returned in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,15 @@
 class SearchController < ApplicationController
 
   def index
-    zip_code = params["zip"]
+    zip_code = params["zip_code_search"]
     connection = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip_code},25))?format=json&show=city,distance,longName,phone,storeType&pageSize=15&apiKey=#{ENV['BEST_BUY_API_KEY']}")
-    JSON.parse(connection.body)
+    output = JSON.parse(connection.body)['stores']
+    @stores = output.map do |store|
+      store['city']
+      store['distance']
+      store['longName']
+      store['phone']
+      store['storeType']
+    end
   end
 end
